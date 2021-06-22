edad5b4349dcd18c819b0b03eb6989668e0f9b84
Bilal <bilal-tariq@live.com>
2021-06-22 16:31:47 +0800

    added data files

---
+++ src/data/master.js
@@ -0,0 +1,124 @@
+export default [
+  {
+    "gtin": "8886008101336",
+    "productName": "Aqua Botol 330ml",	
+    "targetMarkets": ["ID"],
+    "languages": [
+      {
+      "language": "Indonesian",
+      "code": "id-ID"
+      }		
+    ],			
+    "categories": [
+      {
+        "level": 1,
+        "code": "50000000",
+        "description": "Food/Beverage/Tobacco"
+      },
+      {
+        "level": 2,
+        "code": "50200000",
+        "description": "Beverages"
+      },
+      {
+        "level": 3,
+        "code": "50202300",
+        "description": "Non Alcoholic Beverages – Ready to Drink"
+      },
+      {
+        "level": 4,
+        "code": "10000232",
+        "description": "Packaged Water"
+      }
+    ],
+    "manufacturerName": "Danone",
+    "brandName" : "Aqua",
+    "subbrandName" : "",
+    "productPackSize" : "330 ml",
+    "productPackType" : "Bottle",
+    "metricValue" : 0.33,
+    "metricUnit" : "Litres",
+    "isMultiPack" : false,
+    "groupingSets": [
+      {
+        "name": "Product Attributes",
+        "attributes" : 
+        {
+          "productType" : "Market Small Bottle (0-500ml)",
+          "marketSegment" : "Market SPS",
+          "volumeInLiter" : "0.33L",
+          "package": "Small Bottle",
+          "organic":  "Sparkling Water"
+        }
+      }
+    ],
+    "images" : [
+      {
+        "mimeType": "image/jpeg",
+        "measurementUnit": "px",
+        "url" : "https://www.static-src.com/wcsstore/Indraprastha/images/catalog/full//92/MTA-3325718/aqua_aqua-air-mineral-botol--330-ml-_full02.jpg"
+      },
+    ]
+  },
+  {
+    "gtin": "8886008101336",
+    "productName": "Aqua Botol 330ml",	
+    "targetMarkets": ["ID"],
+    "languages": [
+      {
+      "language": "Indonesian",
+      "code": "id-ID"
+      }		
+    ],			
+    "categories": [
+      {
+        "level": 1,
+        "code": "50000000",
+        "description": "Food/Beverage/Tobacco"
+      },
+      {
+        "level": 2,
+        "code": "50200000",
+        "description": "Beverages"
+      },
+      {
+        "level": 3,
+        "code": "50202300",
+        "description": "Non Alcoholic Beverages – Ready to Drink"
+      },
+      {
+        "level": 4,
+        "code": "10000232",
+        "description": "Packaged Water"
+      }
+    ],
+    "manufacturerName": "Danone",
+    "brandName" : "Aqua",
+    "subbrandName" : "",
+    "productPackSize" : "330 ml",
+    "productPackType" : "Bottle",
+    "metricValue" : 0.33,
+    "metricUnit" : "Litres",
+    "isMultiPack" : false,
+    "groupingSets": [
+      {
+        "name": "Product Attributes",
+        "attributes" : 
+        {
+          "productType" : "Market Small Bottle (0-500ml)",
+          "marketSegment" : "Market SPS",
+          "volumeInLiter" : "0.33L",
+          "package": "Small Bottle",
+          "organic":  "Sparkling Water"
+        }
+      }
+    ],
+    "images" : [
+      {
+        "mimeType": "image/jpeg",
+        "measurementUnit": "px",
+        "url" : "https://www.static-src.com/wcsstore/Indraprastha/images/catalog/full//92/MTA-3325718/aqua_aqua-air-mineral-botol--330-ml-_full02.jpg"
+      },
+    ]
+  },
+]
